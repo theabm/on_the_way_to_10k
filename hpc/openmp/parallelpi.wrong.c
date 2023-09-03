4b2c30406ab9d93d14c5526b1a60422ff2d142e9
theabm <andres_bermeo@outlook.com>
2023-09-03 23:34:49 +0200

    small additions

---
+++ hpc/openmp/parallelpi.wrong.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <omp.h>
+#define NUM_THREADS 2
+
+static long num_steps = 1000000000;
+double step;
+int main()
+{
+    double pi = 0.0;
+    int nthreads;
+    double t0 = omp_get_wtime();
+
+    step = 1.0/(double)num_steps;
+
+    // we *request* these threads, but the OS can give us less!
+    omp_set_num_threads(NUM_THREADS);
+
+    double partial_sum[NUM_THREADS];
+
+    #pragma omp parallel
+    {
+        double x = 0.0;
+        int i;
+
+        // we check since it is not guaranteed that 
+        // we get 10 threads
+        int num_threads = omp_get_num_threads();
+        // I get my id
+        int my_id = omp_get_thread_num();
+
+        // the master thread updates the shared variable 
+        if(my_id==0) nthreads = num_threads;
+
+        for (i=my_id, partial_sum[my_id] = 0.0; i<num_steps;i+=num_threads){
+            x = (i+0.5)*step;
+            partial_sum[my_id] += 4.0/(1.0+x*x);
+        }
+    }
+    for(int i = 0; i<NUM_THREADS;++i){
+        pi+=partial_sum[i]*step;
+    }
+    double t1 = omp_get_wtime();
+
+    printf("pi is %f\n", pi);
+    printf("execution time is %f s \n", t1-t0);
+}
---
+++ hpc/openmp/serialpi.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <omp.h>
+static long num_steps = 1000000000;
+double step;
+int main()
+{
+    int i;
+    double x, pi, sum = 0.0;
+    double t0 = omp_get_wtime();
+
+    step = 1.0/(double)num_steps;
+    for (i=0; i<num_steps;i++){
+        x = (i+0.5)*step;
+        sum += 4.0/(1.0+x*x);
+    }
+    pi = step * sum;
+    double t1 = omp_get_wtime();
+
+    printf("pi is %f\n", pi);
+    printf("execution time is %f s \n", t1-t0);
+}
